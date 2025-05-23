2af06d539fb08ba67eec0ae6ece3828cf3b79f7e
madhav <109799001+madhav037@users.noreply.github.com>
2025-05-23 16:13:21 +0530

    changed logger-6

--- api/index.js
+++ api/index.js
@@ -78,6 +78,11 @@ client.on("error", (err) => {
     app.use("/api/admin", AdminHandler);
     app.use("/api/auth", AuthHandler);
 
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    });
+
     app.listen(5000, () => {
       console.log("Server listening on port : 5000");
     });
@@ -107,9 +112,4 @@ cron.schedule("0 0 * * *", async () => {
   console.log("running cron");
 
   await sendLetter();
-});
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
